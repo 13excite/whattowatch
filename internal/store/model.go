a7fc30717871d4287ae01fdb8e7c372d73935e50
Vladimir Dulenov <v.dulenov@ntechlab.com>
2021-09-29 19:50:32 +0300

    Module store updated. Unused method deleted. Method GetRandomFilm logic updated

--- internal/store/model.go
+++ internal/store/model.go
@@ -1,19 +1,11 @@
 package store
 
-import (
-	"context"
-	"database/sql"
-	"fmt"
-	"github.com/lib/pq"
-	"log"
-)
-
 type FilmData struct {
 	Title      string
 	Genre      string
-	PosterLink string
-	RatingKp   []string
-	RatingImdb []string
+	PosterLink string `json:"poster_link,omitempty" db:"poster_link"`
+	RatingKp   []string `json:"rating_kp,omitempty" db:"rating_kp""`
+	RatingImdb []string `json:"rating_imdb,omitempty" db:"rating_imdb"`
 	Country    []string
 	LinkToKP   string
 }
@@ -21,56 +13,4 @@ type FilmData struct {
 type MinMaxIds struct {
 	Min int
 	Max int
-}
-
-func (ids *MinMaxIds) getMinMaxIds(db *sql.DB) (*MinMaxIds, error) {
-	ctx := context.Background()
-	err := db.PingContext(ctx)
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-	query := "select min(film_id), max(film_id) from films"
-	rows, err := db.QueryContext(ctx, query)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	minMaxArr := make([]*MinMaxIds, 1, 1)
-
-	defer rows.Close()
-
-	for rows.Next() {
-		err := rows.Scan(&ids.Min, &ids.Max)
-		if err != nil {
-			return nil, err
-		}
-		minMaxArr = append(minMaxArr, ids)
-	}
-	return minMaxArr[1], nil
-
-}
-
-func (f *FilmData) getRandomFilm(db *sql.DB, randomFilmId int) (*FilmData, error) {
-	ctx := context.Background()
-	err := db.PingContext(ctx)
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-	statement := fmt.Sprintf("SELECT title,genre,poster_link,rating_kp,rating_imdb,country,linktokp  FROM films WHERE film_id=%d", randomFilmId)
-	rows, err := db.QueryContext(ctx, statement)
-
-	filmsArr := make([]*FilmData, 1, 1)
-	defer rows.Close()
-
-	for rows.Next() {
-		err := rows.Scan(&f.Title, &f.Genre, &f.PosterLink,
-			pq.Array(&f.RatingKp), pq.Array(&f.RatingImdb), pq.Array(&f.Country), &f.LinkToKP)
-		if err != nil {
-			return nil, err
-		}
-		filmsArr = append(filmsArr, f)
-	}
-	return filmsArr[1], nil
 }
--- internal/store/postgres/film.go
+++ internal/store/postgres/film.go
@@ -4,20 +4,34 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"github.com/lib/pq"
 	"ww/internal/store"
 )
 
 func (c *Client) GetRandomFilm(ctx context.Context, randomFilmId int) (*store.FilmData, error) {
 	film := new(store.FilmData)
-	statement := fmt.Sprintf("SELECT title,genre,poster_link,rating_kp,rating_imdb,country,linktokp "+
+
+	statement := fmt.Sprintf("SELECT title,genre,poster_link,rating_kp,rating_imdb,country,linktokp  " +
 		"FROM films WHERE film_id=%d", randomFilmId)
-	err := c.db.GetContext(ctx, film, statement)
+	rows, err := c.db.QueryContext(ctx, statement)
 	if err == sql.ErrNoRows {
 		return nil, store.ErrNotFound
 	} else if err != nil {
 		return nil, wrapError(err)
 	}
-	return film, nil
+
+	filmsArr := make([]*store.FilmData, 1, 1)
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&film.Title, &film.Genre, &film.PosterLink,
+			pq.Array(&film.RatingKp), pq.Array(&film.RatingImdb), pq.Array(&film.Country), &film.LinkToKP)
+		if err != nil {
+			return nil, wrapError(err)
+		}
+		filmsArr = append(filmsArr, film)
+	}
+	return filmsArr[1], nil
 }
 
 func (c *Client) GetMinMaxIds(ctx context.Context) (*store.MinMaxIds, error) {
