d9f65fd3277e5c6d300b2cd0dc17c448950563a5
Vladimir Dulenov <originalblack@mail.ru>
2021-10-03 21:48:12 +0300

    FilmData model json fields fixed

--- internal/store/model.go
+++ internal/store/model.go
@@ -1,13 +1,13 @@
 package store
 
 type FilmData struct {
-	Title      string
-	Genre      string
-	PosterLink string `json:"poster_link,omitempty" db:"poster_link"`
+	Title      string   `json:"title"`
+	Genre      string   `json:"genre"`
+	PosterLink string   `json:"poster_link,omitempty" db:"poster_link"`
 	RatingKp   []string `json:"rating_kp,omitempty" db:"rating_kp""`
 	RatingImdb []string `json:"rating_imdb,omitempty" db:"rating_imdb"`
-	Country    []string
-	LinkToKP   string
+	Country    []string `json:"country"`
+	LinkToKP   string   `json:"link_to_kp"`
 }
 
 type MinMaxIds struct {
