c71634c7acc74a9e22e7ba7585b326e7faae9e8a
Vladimir Dulenov <originalblack@mail.ru>
2021-10-28 17:19:26 +0300

    Static updated. Structure changed

--- static/src/main.js
+++ static/src/main.js
@@ -1,11 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-Vue.use(BootstrapVue)
 import 'bootstrap/dist/css/bootstrap.css'
 
+Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+//  router,
+  components: { App },
+  template: '<App/>'
 })
